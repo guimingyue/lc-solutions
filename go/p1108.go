9b740b37906e89ac8bc99bc6ecbd425a9bff4049
guimy <guimingyue@gmail.com>
2022-06-26 11:07:13 +0800

    add p1108

---
+++ go/p1108.go
@@ -0,0 +1,11 @@
+func defangIPaddr(address string) string {
+    str := strings.Builder{}
+	for _, ch := range address {
+		if ch == '.' {
+			str.WriteString("[.]")
+		} else {
+			str.WriteByte(byte(ch))
+		}
+	}
+	return str.String()
+}
